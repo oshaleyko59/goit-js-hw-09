3ff8600978d1c41a243d88c911b190848c98df0f
oshaleyko59 <114358209+oshaleyko59@users.noreply.github.com>
2023-03-05 19:10:09 +0000

    Deploying to gh-pages from @ oshaleyko59/goit-js-hw-09@53bfde41674ccddceac7c72c62b127e0cc68c287 🚀

---
+++ 01-color-switcher.5c329ac1.js
@@ -0,0 +1,2 @@
+const[e,t]=document.querySelectorAll("button");e.classList.add("centered"),t.disabled=!0,e.addEventListener("click",(function({target:d}){e.disabled=!0,t.disabled=!1,l(d.parentElement),n=setInterval((()=>l(d.parentElement)),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(n)}));let n=null;function l(e){e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}
+//# sourceMappingURL=01-color-switcher.5c329ac1.js.map
